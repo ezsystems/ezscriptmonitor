87b19b2f21c933c86e2237c3469890b597ea203d
Jérôme Renard <jr@ez.no>
2010-03-08 13:56:46 +0000

    - Updated postgres schema
    git-svn-id: http://svn.projects.ez.no/ezscriptmonitor/trunk@33 3eb32d69-92b4-4390-8c47-d6f56379109b

--- sql/postgresql/schema.sql
+++ sql/postgresql/schema.sql
@@ -1,5 +1,12 @@
+CREATE SEQUENCE ezscheduled_script_s
+    START 1
+    INCREMENT 1
+    MAXVALUE 9223372036854775807
+    MINVALUE 1
+    CACHE 1;
+
 CREATE TABLE ezscheduled_script (
-  ID                    SERIAL   NOT NULL,
+  id                    integer DEFAULT nextval('ezscheduled_script_s'::text) NOT NULL,
   process_id            INT   DEFAULT '0'   NOT NULL,
   NAME                  VARCHAR(50)   DEFAULT ''   NOT NULL,
   command               VARCHAR(255)   NOT NULL,
@@ -10,3 +17,5 @@ CREATE TABLE ezscheduled_script (
 
 CREATE INDEX ezscheduled_script_timestamp ON ezscheduled_script (
       last_report_timestamp);
+
+SELECT setval('ezscheduled_script_s', max(id)) , 'ezscheduled_script' as tablename FROM ezscheduled_script;
