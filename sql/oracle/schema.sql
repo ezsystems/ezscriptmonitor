47e9219f2624e308871d7f85b24ca58074710d59
Jérôme Renard <jr@ez.no>
2010-03-08 12:35:18 +0000

    - Update trigger syntax
    git-svn-id: http://svn.projects.ez.no/ezscriptmonitor/trunk@32 3eb32d69-92b4-4390-8c47-d6f56379109b

--- sql/oracle/schema.sql
+++ sql/oracle/schema.sql
@@ -8,14 +8,15 @@ CREATE TABLE ezscheduled_script (
   user_id               INT    DEFAULT 0    NOT NULL,
   CONSTRAINT pk_ezscheduled_script PRIMARY KEY ( id ));
 
-CREATE INDEX ezscheduled_script_timestamp ON ezscheduled_script (
-      last_report_timestamp);
+CREATE INDEX ezscheduled_script_timestamp ON ezscheduled_script (last_report_timestamp);
 
 CREATE SEQUENCE s_ezscheduled_script;
 
 CREATE OR REPLACE TRIGGER ezscheduled_script_id_tr
-BEFORE INSERT ON ezscheduled_script FOR EACH ROW WHEN ( NEW.id IS NULL )
-BEGIN
-  SELECT seq_ezscheduled_script.nextval INTO :new.id FROM dual;
-END;
-/ 
+   BEFORE INSERT ON ezscheduled_script FOR EACH ROW WHEN ( NEW.id IS NULL )
+   BEGIN
+      SELECT seq_ezscheduled_script.nextval
+      INTO :new.id
+      FROM dual;
+   END;
+/
