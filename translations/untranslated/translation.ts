60a58c2286cbb7a9078a6270992200a23d290e21
crevillo <crevillo@gmail.com>
2011-03-08 20:56:28 +0100

    localization: added untranslated ts file

---
+++ translations/untranslated/translation.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>ezscriptmonitor</name>
+    <message>
+        <location filename="ezscheduledscript.php" line="123"/>
+        <source>n/a</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ezscheduledscript.php" line="163"/>
+        <location line="0"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="list.php" line="22"/>
+        <location filename="view.php" line="27"/>
+        <source>Script monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>These are the pending, currently executing or recently finished scripts.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current scripts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Script ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scheduled by</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not started</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Waiting for  update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dead</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Script monitor:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Process ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The script does not exist!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="view.php" line="17"/>
+        <source>Script not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
