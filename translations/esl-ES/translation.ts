ef09b9e503cc1a59d9ae6c080f00209117d17839
crevillo <crevillo@gmail.com>
2011-03-08 21:01:43 +0100

    localization: added esl-ES translation.ts

---
+++ translations/esl-ES/translation.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>ezscriptmonitor</name>
+    <message>
+        <location filename="ezscheduledscript.php" line="123"/>
+        <source>n/a</source>
+        <translation>n/a</translation>
+    </message>
+    <message>
+        <location filename="ezscheduledscript.php" line="163"/>
+        <location filename="ezscheduledscript.php" line="0"/>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+    <message>
+        <location filename="list.php" line="22"/>
+        <location filename="view.php" line="27"/>
+        <source>Script monitor</source>
+        <translation>Monitor de scripts</translation>
+    </message>
+    <message>
+        <source>These are the pending, currently executing or recently finished scripts.</source>
+        <translation>Estos son los scripts que están pendientes, actualmente ejecutándose o recientemente finalizados.</translation>
+    </message>
+    <message>
+        <source>Refresh page</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <source>Current scripts</source>
+        <translation>Script actuales</translation>
+    </message>
+    <message>
+        <source>Script ID</source>
+        <translation>ID del Script</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <source>Scheduled by</source>
+        <translation>Programado por</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <source>Last report</source>
+        <translation>Último informe</translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation>Progreso</translation>
+    </message>
+    <message>
+        <source>Not started</source>
+        <translation>Sin comenzar</translation>
+    </message>
+    <message>
+        <source>Active</source>
+        <translation>Activo</translation>
+    </message>
+    <message>
+        <source>Waiting for  update</source>
+        <translation>Esperando actualización</translation>
+    </message>
+    <message>
+        <source>Dead</source>
+        <translation>Muerto</translation>
+    </message>
+    <message>
+        <source>Complete</source>
+        <translation>Completado</translation>
+    </message>
+    <message>
+        <source>Script monitor:</source>
+        <translation>Monitor de scripts:</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>Datos</translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation>Valor</translation>
+    </message>
+    <message>
+        <source>Process ID</source>
+        <translation>ID de proceso</translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation>Comando</translation>
+    </message>
+    <message>
+        <source>The script does not exist!</source>
+        <translation>¡El script no existe!</translation>
+    </message>
+    <message>
+        <location filename="view.php" line="17"/>
+        <source>Script not found</source>
+        <translation>Script no encontrado</translation>
+    </message>
+</context>
+</TS>
