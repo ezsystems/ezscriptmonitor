85c134c843a3f835a82a63262310b2ac1649bf5e
ballinette <gballini@kaliop.com>
2013-07-25 12:00:39 +0200

    adding comment on ObjectLimit setting

--- settings/ezscriptmonitor.ini.append.php
+++ settings/ezscriptmonitor.ini.append.php
@@ -3,6 +3,9 @@
 [GeneralSettings]
 ScriptSiteAccess=ezwebin_site_admin
 PhpCliCommand=/usr/bin/php
+
+# Max number of objects fetched at a time while updating class attributes :
+# the script will iterate by packets of <ObjectLimit> objects
 ObjectLimit=500
 
 */ ?>
