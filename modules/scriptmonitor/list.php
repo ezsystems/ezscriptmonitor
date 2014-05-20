4c682951dcbc6dd33eaf17ea03271c7cb6687ede
André R <ar@ez.no>
2014-05-20 22:01:07 +0200

    Fix EZP-22128: Update copyright info on old headers

--- modules/scriptmonitor/list.php
+++ modules/scriptmonitor/list.php
@@ -2,7 +2,7 @@
 /**
  * File containing list view
  *
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) eZ Systems AS.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  *
  */
